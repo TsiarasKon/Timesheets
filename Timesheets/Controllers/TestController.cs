ad8efc45199b6f3f9be8c692ecd3d89d26bafeda
Konstantinos Tsiaras <tsiaraskon@gmail.com>
2019-12-02 20:18:07 +0200

    TestController and View to validate that Projects-Departments relationships work as expected

---
+++ Timesheets/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Timesheets.Data;
+using Timesheets.Models;
+
+namespace Timesheets.Controllers
+{
+    public class TestController : Controller
+    {
+
+        private ApplicationDbContext _context;
+
+        public TestController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            TestPageModel model = new TestPageModel();
+            model.Projects = _context.Projects.ToList();
+            model.Departments = _context.Departments.ToList();
+            model.DepartmentProjects = _context.DepartmentProjects.ToList();
+            return View(model);
+        }
+    }
+}
---
+++ Timesheets/Models/TestPageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Timesheets.Models
+{
+    public class TestPageModel
+    {
+        public IEnumerable<Project> Projects { get; set; }
+        public IEnumerable<Department> Departments { get; set; }
+        public IEnumerable<DepartmentProject> DepartmentProjects { get; set; }
+    }
+}
