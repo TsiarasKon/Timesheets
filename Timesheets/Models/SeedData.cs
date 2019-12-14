151dad260541c41a0dc31a54fbc27528e450a481
tra3dimi <stratigopoulou_dimitra@yahoo.com>
2019-12-14 11:43:01 +0200

    seed data added

--- Timesheets/Models/SeedData.cs
+++ Timesheets/Models/SeedData.cs
@@ -17,14 +17,15 @@ namespace Timesheets.Models
 
         public static void SeedUsers(UserManager<ApplicationUser> userManager)
         {
-            var username1 = "a@a.a";
+            var username1 = "petroula@a.a";
             if (userManager.FindByNameAsync(username1).Result == null)
             {
-                ApplicationUser user = new ApplicationUser(){
+                ApplicationUser user = new ApplicationUser()
+                {
                     UserName = username1,
                     Email = username1,
-                    FirstName = "That",
-                    LastName = "Guy",
+                    FirstName = "Petroula",
+                    LastName = "Stamouli",
                     ManHourCost = 3.14,
                     EmailConfirmed = true
                 };
@@ -35,15 +36,15 @@ namespace Timesheets.Models
                 //    userManager.AddToRoleAsync(user, "NormalUser").Wait();
                 //}
             }
-            var username2 = "b@b.b";
+            var username2 = "sofia@b.b";
             if (userManager.FindByNameAsync(username2).Result == null)
             {
                 ApplicationUser user = new ApplicationUser()
                 {
                     UserName = username2,
                     Email = username2,
-                    FirstName = "Other",
-                    LastName = "Guy",
+                    FirstName = "Sofia",
+                    LastName = "Tseranidou",
                     ManHourCost = 5,
                     EmailConfirmed = true
                 };
@@ -54,15 +55,15 @@ namespace Timesheets.Models
                 //    userManager.AddToRoleAsync(user, "NormalUser").Wait();
                 //}
             }
-            var username3 = "c@c.c";
+            var username3 = "nikos@c.c";
             if (userManager.FindByNameAsync(username3).Result == null)
             {
                 ApplicationUser user = new ApplicationUser()
                 {
                     UserName = username3,
                     Email = username3,
-                    FirstName = "C",
-                    LastName = "CC",
+                    FirstName = "Nikos",
+                    LastName = "Stavrou",
                     ManHourCost = 4,
                     EmailConfirmed = true
                 };
@@ -73,15 +74,15 @@ namespace Timesheets.Models
                 //    userManager.AddToRoleAsync(user, "NormalUser").Wait();
                 //}
             }
-            var username4 = "d@d.d";
+            var username4 = "antonis@d.d";
             if (userManager.FindByNameAsync(username4).Result == null)
             {
                 ApplicationUser user = new ApplicationUser()
                 {
                     UserName = username4,
                     Email = username4,
-                    FirstName = "D",
-                    LastName = "DC",
+                    FirstName = "Antonis",
+                    LastName = "Fragkiadakis",
                     ManHourCost = 2,
                     EmailConfirmed = true
                 };
@@ -107,11 +108,14 @@ namespace Timesheets.Models
 
                 var usersIdList = context.ApplicationUsers.Select(x => x.Id).Distinct().ToList();
 
-                IList<Department> newDepartments = new List<Department>() 
+                IList<Department> newDepartments = new List<Department>()
                 {
-                    new Department() { Name = "Dept1", DepartmentHeadId = usersIdList[0] },
-                    new Department() { Name = "Dept2", DepartmentHeadId = usersIdList[2] },
-                    new Department() { Name = "Dept3", DepartmentHeadId = usersIdList[1] }
+                    new Department() { Name = "Banking", DepartmentHeadId = usersIdList[0] },
+                    new Department() { Name = "Infrastructure", DepartmentHeadId = usersIdList[2] },
+                    new Department() { Name = "Networking", DepartmentHeadId = usersIdList[1] },
+                    new Department() { Name = "Telecommunications", DepartmentHeadId = usersIdList[2] },
+                    new Department() { Name = "Finance", DepartmentHeadId = usersIdList[1] }
+
                 };
                 context.Departments.AddRange(newDepartments);
                 context.SaveChanges();
@@ -120,10 +124,12 @@ namespace Timesheets.Models
 
                 IList<Project> newProjects = new List<Project>()
                 {
-                    new Project() { Name = "Proj1", OwnerDepartmentId = newDepartmentsIdList[0] },
-                    new Project() { Name = "Proj2", OwnerDepartmentId = newDepartmentsIdList[1] },
-                    new Project() { Name = "Proj3", OwnerDepartmentId = newDepartmentsIdList[0] },
-                    new Project() { Name = "Proj4", OwnerDepartmentId = newDepartmentsIdList[2] }
+                    new Project() { Name = "Android App For New Product", OwnerDepartmentId = newDepartmentsIdList[0] },
+                    new Project() { Name = "New Website", OwnerDepartmentId = newDepartmentsIdList[1] },
+                    new Project() { Name = "New database", OwnerDepartmentId = newDepartmentsIdList[0] },
+                    new Project() { Name = "Create a website for the product.", OwnerDepartmentId = newDepartmentsIdList[2] },
+                    new Project() { Name = "Integrate sql db.", OwnerDepartmentId = newDepartmentsIdList[1] }
+
                 };
                 context.Projects.AddRange(newProjects);
                 context.SaveChanges();
@@ -147,9 +153,18 @@ namespace Timesheets.Models
                 IList<TimesheetEntry> newTimesheetEntries = new List<TimesheetEntry>()
                 {
                     new TimesheetEntry() { DateCreated = DateTime.Now, HoursWorked = 7, UserId = usersIdList[0], ProjectId = newProjectsIdList[0] },
-                    new TimesheetEntry() { DateCreated = DateTime.Now, HoursWorked = 3, UserId = usersIdList[0], ProjectId = newProjectsIdList[1] }
+                    new TimesheetEntry() { DateCreated = DateTime.Now, HoursWorked = 3, UserId = usersIdList[0], ProjectId = newProjectsIdList[1] },
+                    new TimesheetEntry() { DateCreated = DateTime.Now, HoursWorked = 8, UserId = usersIdList[2], ProjectId = newProjectsIdList[3] },
+                    new TimesheetEntry() { DateCreated = DateTime.Now, HoursWorked = 10,UserId = usersIdList[1], ProjectId = newProjectsIdList[2] },
+                    new TimesheetEntry() { DateCreated = DateTime.Now, HoursWorked = 4,UserId = usersIdList[2], ProjectId = newProjectsIdList[3] },
+                    new TimesheetEntry() { DateCreated = DateTime.Now, HoursWorked = 9,UserId = usersIdList[1], ProjectId = newProjectsIdList[1] },
+                    new TimesheetEntry() { DateCreated = DateTime.Now, HoursWorked = 12,UserId = usersIdList[1], ProjectId = newProjectsIdList[2] },
+                    new TimesheetEntry() { DateCreated = DateTime.Now, HoursWorked = 13,UserId = usersIdList[0], ProjectId = newProjectsIdList[1] },
+                    new TimesheetEntry() { DateCreated = DateTime.Now, HoursWorked = 14,UserId = usersIdList[0], ProjectId = newProjectsIdList[2] },
+                    new TimesheetEntry() { DateCreated = DateTime.Now, HoursWorked = 20,UserId = usersIdList[1], ProjectId = newProjectsIdList[3] }
                 };
                 context.TimesheetEntries.AddRange(newTimesheetEntries);
+              
                 context.SaveChanges();
 
                 // TODO: insert more data; update user DepartmentIds
